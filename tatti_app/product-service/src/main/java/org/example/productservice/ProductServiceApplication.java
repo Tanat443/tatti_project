515e6e570b6a248d99788eb4aa6d5fa64d248404
bakiir <bkiranarbek@gmail.com>
2025-04-30 20:25:17 +0600

    Add: flyway migration, Dockerfiles

--- tatti_app/product-service/src/main/java/org/example/productservice/ProductServiceApplication.java
+++ tatti_app/product-service/src/main/java/org/example/productservice/ProductServiceApplication.java
@@ -2,12 +2,13 @@ package org.example.productservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class ProductServiceApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(ProductServiceApplication.class, args);
     }
 
---
+++ tatti_app/product-service/src/main/resources/db/migrations/V1__init.sql
@@ -0,0 +1,18 @@
+CREATE TABLE categories (
+                            id BIGSERIAL PRIMARY KEY,
+                            name VARCHAR(255) NOT NULL
+);
+CREATE TABLE products (
+                          id BIGSERIAL PRIMARY KEY,
+                          name VARCHAR(255) NOT NULL,
+                          description TEXT,
+                          price DOUBLE PRECISION NOT NULL,
+                          cpfc VARCHAR(255),
+                          photo VARCHAR(255),
+                          weight DOUBLE PRECISION,
+                          category_id BIGINT REFERENCES categories(id),
+                          created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+                          updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+
+
