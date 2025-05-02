2110cfa678f3700328eae026ba953bf410e6cbf1
w2 <tanat443@gmail.com>
2025-05-03 01:15:48 +0500

    added flyway migrations, and created dockerfiles, changed keycloak secret, added action file to CI/CD

--- tatti_app/order-service/src/main/java/org/example/orderservice/repository/OrderRepository.java
+++ tatti_app/order-service/src/main/java/org/example/orderservice/repository/OrderRepository.java
@@ -3,10 +3,12 @@ package org.example.orderservice.repository;
 import org.example.orderservice.model.Order;
 import org.example.orderservice.model.OrderStatus;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.stereotype.Repository;
 import reactor.core.publisher.Flux;
 
 import java.time.LocalDateTime;
 
+@Repository
 public interface OrderRepository extends ReactiveCrudRepository<Order, Long> {
     Flux<Order> findAllByCustomerId(Long customerId);
     Flux<Order> findAllByAssignedConfectionerId(Long confectionerId);
